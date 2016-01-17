414400cab4836a49cde1947af05731ffbc163c57
xzk0701 <xzk0701@qq.com>
2016-01-17 09:56:13 +0800

    changes

--- installss.sh
+++ installss.sh
@@ -1,6 +1,5 @@
 clear
 echo "#########################################"
-wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks.sh
 chmod +x shadowsocks.sh
 ./shadowsocks.sh 2>&1 | tee shadowsocks.log
 
